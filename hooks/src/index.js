51363bfd454b7424886754597aa8b4b8942b1812
Loki <zpk.loki@gmail.com>
2020-01-20 13:36:40 +0300

    hooks

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => <p>Hi!</p>
+const App = () => <p>Hi!</p>;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
