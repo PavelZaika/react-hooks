f96f2bd7651182468a079ec7a16ad0904232b464
Loki <zpk.loki@gmail.com>
2020-01-22 16:54:28 +0300

    useState

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <p>Hi!</p>;
+const App = () => {
+  return (
+    <div>
+      <HookSwitcher />
+    </div>
+  );
+};
+
+const HookSwitcher = () => {
+  const [color, setColor] = useState("white");
+  const [fontSize, setFontSize] = useState(14);
+  return (
+    <div style={{ padding: "10px", backgroundColor: color, fontSize: `${fontSize}px`}}>
+    Hello World!!!
+      <button onClick={() => setColor("gray")}> Dark </button>
+      <button onClick={() => setColor("white")}>Light</button>
+      <button onClick={() => setFontSize((s) => s + 2)}>Font Size</button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
