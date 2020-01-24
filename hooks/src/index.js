e9f5592a6a64edc53ec69aced5fd47801d4fc7fd
Loki <zpk.loki@gmail.com>
2020-01-24 17:18:13 +0300

    useEffect and upload data

---
+++ hooks/src/index.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [value, setValue] = useState(1);
+  const [visible, setVisible] = useState(true);
+
+  if (visible) {
+    return (
+      <div>
+        <button onClick={() => setValue(v => v + 1)}>+</button>
+        <button onClick={() => setVisible(false)}>Hide</button>
+        <PlanetInfo id={value} />
+      </div>
+    );
+  } else {
+    return <button onClick={() => setVisible(true)}>Show</button>;
+  }
+};
+
+const PlanetInfo = ({ id }) => {
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    let canceled = false;
+    fetch(`https://swapi.co/api/planets/${id}/`)
+      .then(res => res.json())
+      .then(data => !canceled && setName(data.name));
+      return () => canceled = true;
+  },[id]);
+
+  return (
+    <div>
+      {id} - {name}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
